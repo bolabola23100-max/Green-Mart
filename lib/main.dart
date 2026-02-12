10107422431f9a5f6f4bd2596055f806a4619bf9
Bibo <bolaatef20061@gmail.com>
2026-02-12 16:41:26 +0200

    new file:   lib/App/MyApp.dart     modified:   lib/main.dart     deleted:    test/widget_test.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:green_mart/core/theme/colors.dart';
-import 'package:green_mart/features/home/screens/splash_screen.dart';
+import 'package:green_mart/App/MyApp.dart';
 
 void main() {
   runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.backgroundColor,
-
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: AppColors.primaryColor,
-
-            minimumSize: Size(319, 58),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20),
-            ),
-            foregroundColor: AppColors.backgroundColor,
-          ),
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(22),
-            borderSide: BorderSide.none,
-          ),
-          hintStyle: TextStyle(color: AppColors.greyColor),
-          fillColor: AppColors.accentColor,
-          filled: true,
-        ),
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: AppColors.primaryColor,
-          onSurface: AppColors.blackColor,
-        ),
-      ),
-      builder: (context, child) {
-        return SafeArea(
-          top: false,
-          bottom: Platform.isAndroid,
-          child: child ?? Scaffold(),
-        );
-      },
-
-      home: SplashScreen(),
-    );
-  }
 }
